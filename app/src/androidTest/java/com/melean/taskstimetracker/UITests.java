5eeb3f4110706387958e4f1169313d860261a6d1
MDDimitrov <milen820430@gmail.com>
2017-03-10 10:15:03 +0200

    start testing ui

--- app/src/androidTest/java/com/melean/taskstimetracker/UITests.java
+++ app/src/androidTest/java/com/melean/taskstimetracker/UITests.java
@@ -8,6 +8,7 @@ import android.support.test.runner.AndroidJUnit4;
 import com.melean.taskstimetracker.recordTasks.RecordTaskActivity;
 
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,20 +20,33 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class UITests {
+    private Resources res;
+
     @Rule
     public ActivityTestRule<RecordTaskActivity> testedActivity =
             new ActivityTestRule<>(RecordTaskActivity.class);
 
+    @Before
+    public void initData(){
+         res = testedActivity.getActivity().getResources();
+    }
+
+    @Test
+    @LargeTest
+    public void CheckStartingViewsLoaded(){
+        onView(allOf(withId(R.id.tasks_list))).check(matches(isDisplayed()));
+    }
+
     @Test
     @LargeTest
     public void clickStartRecord_PauseButtonAndStartStopToggle(){
-        Resources res = testedActivity.getActivity().getResources();
 
         //first click should show pause button and change image to save on record button
-        onView(withId(R.id.fab_record)).perform(click());
+        onView(allOf(withId(R.id.fab_record))).perform(click());
         onView(withId(R.id.fab_pause)).check(matches(isDisplayed()));
         onView(withContentDescription(res.getString(R.string.save))).check(matches(isDisplayed()));
 
